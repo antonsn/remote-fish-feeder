8f73ef1480cd3f682be1354f7430877ecb2e934b
root <anton.snitko@gmail.com>
2021-05-11 22:37:07 +0100

    add mjpeg proxy

--- server.js
+++ server.js
@@ -2,25 +2,27 @@
 const express = require('express')
 const basicAuth = require('express-basic-auth')
 const Gpio = require('onoff').Gpio
+var MjpegProxy = require('mjpeg-proxy').MjpegProxy
+const port = 80
+
 
 
 const app = express()
 
 app.use(basicAuth({
   users: { admin: 'feeder' },
-  challenge: true 
+  challenge: true
 }))
 
 
 app.use(express.static('public'))
 
-
 const feedPin = new Gpio(17, 'out')
-
 feedPin.writeSync(1)
 
- 
 
+//TODO add motion auth
+app.get('/tank.jpg', new MjpegProxy('http://192.168.2.123:8081').proxyRequest);
 
 app.get('/', function (req, res) {
   return res.sendfile('/public/index.html', { root: __dirname + '/..' })
@@ -37,8 +39,8 @@ app.post('/feed', function (req, res) {
   res.send('feeded')
 })
 
-app.listen(80, function () {
-  console.log('app listening')
-}) 
+app.listen(port, function () {
+  console.log(`feeder listening port ${port}`)
+})
 
 
